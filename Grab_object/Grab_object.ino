dc18a4f4df355dead79f326fd9ccab9913bbe7a1
tung1406 <tungvt1406@gmail.com>
2024-01-24 12:51:32 +0700

    update

--- Grab_object/Grab_object.ino
+++ Grab_object/Grab_object.ino
@@ -1,21 +1,21 @@
-#define PinRelay1 7 //dc bom hut
-#define PinRelay2 6 //van khi nen - xylanh
+#define PinRelay1 7 //Vacuum motor
+#define PinRelay2 6 //Solenoid valve
 
-#define PinStep 10
+#define PinStep 10  //Step motor
 #define PinDir 4
 #define PinEn 3
 
-#define IN1 8
+#define IN1 8       //DC motor - L298N
 #define IN2 5
 
 String coordinate = "";
-float distance_offset = 69;           // k/c tu vi tri home den ria khung anh (mm)
-float distance_to_target = 0;         // k/c tu ria khung anh den vi tri cuc ao (mm)
-float distance_resolution = 40;       // k/c di duoc khi quay 1 vong dong co step (mm) 
-float pulse_resolution = 200;         // 1 vong quay step can 200 xung (mm)
-float pixel_to_mm = 0.2844311;          // ty le pixel so voi k/c (mm/pxl) 
-float pixel = 0;                      // toa do x_c cua cuc ao tren anh (pxl)
-int pulse = 0;                        // gia tri xung de dieu khien dong co step
+float distance_offset = 69;           // Distance from home position to the edge of the frame (mm)
+float distance_to_target = 0;         // Distance from the edge of the frame to the button (mm)
+float distance_resolution = 40;       // Distance traveled when step completes one revolution (mm) 
+float pulse_resolution = 200;         // One revolution needs 200 pulses(mm)
+float pixel_to_mm = 0.2844311;        // 1 px = 0.284 mm (mm/pxl) 
+float pixel = 0;                      
+int pulse = 0;                        
 
 void setup() 
 {
@@ -24,8 +24,8 @@ void setup()
  pinMode(IN1, OUTPUT); 
  pinMode(IN2, OUTPUT);
   
-  pinMode(PinRelay1, OUTPUT); // bom hut
-  pinMode(PinRelay2, OUTPUT); // van khi nen
+  pinMode(PinRelay1, OUTPUT); 
+  pinMode(PinRelay2, OUTPUT); 
 
   pinMode(PinDir, OUTPUT);
   pinMode(PinStep, OUTPUT);
@@ -39,27 +39,27 @@ void setup()
   conveyor_on();
 }
 
-void conveyor_on()                    // chay bang tai
+void conveyor_on()                       // on conveyor
 {
  digitalWrite(IN1, HIGH);
  analogWrite(IN2, 127);
 }
 
-// void conveyor_off()                   // dung bang tai
-// {
-//  digitalWrite(IN1, LOW);
-//  analogWrite(IN2, LOW);
-// }
+ void conveyor_off()                   // off conveyor
+ {
+  digitalWrite(IN1, LOW);
+  analogWrite(IN2, LOW);
+ }
 
 void calculate_distance()
 {
-    distance_to_target = pixel_to_mm * pixel;                                                 // chuyen pixel sang mm
-    pulse = (distance_offset + distance_to_target) * pulse_resolution / distance_resolution;  // chuyen mm sang xung 
+    distance_to_target = pixel_to_mm * pixel;                                                 // convert pixel to mm
+    pulse = (distance_offset + distance_to_target) * pulse_resolution / distance_resolution;  // convert mm to pulse 
 }
 
 void grab_object() 
 {
-  // chay step thuan
+  // run step
   digitalWrite(PinDir, HIGH);
   for (int i = 0; i < pulse; i++) 
   {
@@ -69,18 +69,20 @@ void grab_object()
     delayMicroseconds(1000);
   }
 
-  delay(10400);                          // thơi gian doi vat den vị trí hút
+  delay(10400);                          // timming 
 
-  //conveyor_off()
+  conveyor_off()
 
-  digitalWrite(PinRelay1, HIGH);        // bat bom hut
+  digitalWrite(PinRelay1, HIGH);        // on vacuum 
     
-  digitalWrite(PinRelay2, HIGH);        // ha xi lanh
-  delay(100);
-  digitalWrite(PinRelay2, LOW);         // nang xilanh
-  delay(100);
+  digitalWrite(PinRelay2, HIGH);        // lower cylinder
+  delay(500);
+  digitalWrite(PinRelay2, LOW);         // upper cylinder
+  delay(500);
+
+  conveyor_on();
   
-  // chay step nghich
+  // run inverted step
   digitalWrite(PinDir, LOW);
   for (int i = pulse; i > 0; i--) 
   {
@@ -90,12 +92,12 @@ void grab_object()
     delayMicroseconds(1000);
   }
   
-  // conveyor_on();
-  digitalWrite(PinRelay1, LOW);         // tat bom hut
+  
+  digitalWrite(PinRelay1, LOW);         // off vacuum
 
-  digitalWrite(PinRelay2, HIGH);        // ha xi lanh
+  digitalWrite(PinRelay2, HIGH);        // lower cylinder
   delay(500);
-  digitalWrite(PinRelay2, LOW);         // nang xilanh
+  digitalWrite(PinRelay2, LOW);         // upper cylinder
   delay(500);
 
   Serial.print("done");
